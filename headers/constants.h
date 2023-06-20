84ccca31b2744091f2e2c5df9ea85f9092d17f8f
ppaunovski <paunovskipavel@gmail.com>
2023-06-20 16:57:47 +0200

    constants header with useful constants

---
+++ headers/constants.h
@@ -0,0 +1,35 @@
+//
+// Created by pauno on 6/20/2023.
+//
+
+#ifndef RAY_TRACING_IN_C___CONSTANTS_H
+#define RAY_TRACING_IN_C___CONSTANTS_H
+
+#include <cmath>
+#include <limits>
+#include <memory>
+
+
+// Usings
+
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+// Common Headers
+
+#include "ray.h"
+#include "vec3.h"
+
+#endif //RAY_TRACING_IN_C___CONSTANTS_H
