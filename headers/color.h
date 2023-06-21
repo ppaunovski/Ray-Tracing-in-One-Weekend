5dbf16171b1b8e8861526911c1728af39ba76801
ppaunovski <paunovskipavel@gmail.com>
2023-06-21 18:00:45 +0200

    implemented anti-aliasing. added camera class and some utility functions.

--- headers/color.h
+++ headers/color.h
@@ -8,10 +8,20 @@
 #include "vec3.h"
 #include <fstream>
 
-void write_color(std::ofstream &out, color pixel_color){
-    out << static_cast<int>(255.999 * pixel_color.x()) << " "
-        << static_cast<int>(255.999 * pixel_color.y()) << " "
-        << static_cast<int>(255.999 * pixel_color.z()) << "\n";
+void write_color(std::ofstream &out, color pixel_color, int samples_per_pixel){
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    auto scale = 1.0 / samples_per_pixel;
+
+    r *= scale;
+    b *= scale;
+    g *= scale;
+
+    out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << " "
+        << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << " "
+        << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << "\n";
 }
 
 #endif
